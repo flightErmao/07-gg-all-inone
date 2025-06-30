9b286ac3c1824b949e8df98468d2d09c60930e5d
Gaogao <1024355591@qq.com>
2025-06-30 13:48:52 +0800

    加入env参数选择跳转到指定的bsp目录

--- env.bat
+++ env.bat
@@ -1,4 +1,21 @@
 @echo off
 
-REM 自动调用tools/env_win/env.bat
-call tools\env_win\env.bat 
+REM Auto call tools/env_win/env.bat, support selecting project in bsp by argument
+setlocal
+set PROJ=%1
+
+if "%PROJ%"=="" (
+    set BSP_PATH=%~dp0bsp
+) else if "%PROJ%"=="01" (
+    set BSP_PATH=%~dp0bsp\01-minifly-8M
+) else (
+    echo Unknown project number: %PROJ%
+    set BSP_PATH=%~dp0bsp
+    @REM exit /b 1
+)
+
+if not exist "%BSP_PATH%" (
+    echo Path %BSP_PATH% does not exist!
+    exit /b 1
+)
+call tools\env_win\env.bat "%BSP_PATH%" 
