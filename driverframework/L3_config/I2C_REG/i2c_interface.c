a3e05b1cd12d668c759095c22265b0cecaf35ce8
Gaogao <1024355591@qq.com>
2025-07-02 11:08:00 +0800

    优化spl06_reg，但是无法独立两个实例，sdk需要cpp化

--- driverframework/L3_config/I2C_REG/i2c_interface.c
+++ driverframework/L3_config/I2C_REG/i2c_interface.c
@@ -1,11 +1,6 @@
-#include <rtthread.h>
+#include "i2c_interface.h"
 
 #define IIC_MAX_WIRTE_COUNT 512
-
-typedef struct {
-  rt_device_t i2c_dev;
-  uint8_t i2c_addr;
-} I2cInterface_t;
 
 static rt_err_t i2cdevRead(rt_device_t bus, uint8_t devAddress, uint8_t memAddress, uint16_t len, uint8_t* data) {
   struct rt_i2c_msg msg[2] = {0};
@@ -58,12 +53,15 @@ int8_t i2c_read_reg8_mult_pack(I2cInterface_t i2c_interface, uint8_t register_ad
   return ret_function;
 }
 
-static int8_t i2c_read_reg8_pack(uint8_t device_addr, uint8_t register_addr, uint8_t* data, uint8_t len) {
-  int8_t ret_function = i2cdevRead(i2c_dev, device_addr, register_addr, len, data);
+int8_t i2c_write_reg8_mult_pack(I2cInterface_t i2c_interface, uint8_t register_addr, uint8_t* data, uint8_t len) {
+  int8_t ret_function = i2cdevWrite(i2c_interface.i2c_dev, i2c_interface.i2c_addr, register_addr, len, data);
   return ret_function;
 }
 
-static int8_t i2c_write_reg8_pack(uint8_t device_addr, uint8_t register_addr, uint8_t* data, uint8_t len) {
-  int8_t ret_function = i2cdevWrite(i2c_dev, device_addr, register_addr, len, data);
-  return ret_function;
+rt_err_t get_i2c_interface(const char* i2c_device_name, uint8_t i2c_addr, I2cInterface_t* i2c_interface) {
+  i2c_interface->i2c_dev = rt_device_find(i2c_device_name);
+  RT_ASSERT(i2c_interface->i2c_dev != RT_NULL);
+  RT_ASSERT(rt_device_open(i2c_interface->i2c_dev, RT_DEVICE_OFLAG_RDWR) == RT_EOK);
+  i2c_interface->i2c_addr = i2c_addr;
+  return RT_EOK;
 }
