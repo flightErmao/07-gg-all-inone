7f019d0b67cce4e8642cf9edcfc90556236c05b9
Gaogao <1024355591@qq.com>
2025-08-26 11:21:37 +0800

    添加stabilizer的debugPin开关

--- driverframework/L0_task/04_project/01_minifly/TASK_02_STABLIZE/taskMiniflyStabilizer.c
+++ driverframework/L0_task/04_project/01_minifly/TASK_02_STABLIZE/taskMiniflyStabilizer.c
@@ -5,11 +5,16 @@
 #include "sensfusion6.h"
 #include "taskMiniflySensor.h"
 #include "uMCN.h"
+#ifdef PROJECT_MINIFLY_TASK_DEBUGPIN_EN
 #include "debugPin.h"
+#endif
 
 #define THREAD_PRIORITY 5
 #define THREAD_STACK_SIZE 4096
 #define THREAD_TIMESLICE 5
+
+#define STABILIZER_EVENT_FLAG (1u << 0)
+
 static rt_uint8_t task_stack_stabilizer_minifly[THREAD_STACK_SIZE];
 static rt_timer_t stabilizer_timer = RT_NULL;
 static rt_event_t stabilizer_event = RT_NULL;
@@ -35,7 +40,7 @@ static void mcnTopicInit(void) {
   }
 }
 
-static void stabilizer_timer_callback(void* parameter) { rt_event_send(stabilizer_event, 0x01); }
+static void stabilizer_timer_callback(void* parameter) { rt_event_send(stabilizer_event, STABILIZER_EVENT_FLAG); }
 
 void taskStabilizerInit(void) {
   mcnTopicInit();
@@ -57,31 +62,30 @@ void taskStabilizerInit(void) {
 
 static void stabilizer_minifly_thread_entry(void* parameter) {
   uint32_t tick = 0;
-  rt_uint32_t recv_event = 0;
-  rt_err_t result;
   sensorData_t sensorData = {0};
 
-  // 等待传感器校准完成
   while (!sensorsAreCalibrated()) {
-    rt_thread_mdelay(10);
+    rt_thread_mdelay(1000);
   }
   rt_timer_start(stabilizer_timer);
 
   while (1) {
-    DEBUG_PIN_DEBUG3_HIGH();
-    result =
-        rt_event_recv(stabilizer_event, 0x01, RT_EVENT_FLAG_OR | RT_EVENT_FLAG_CLEAR, RT_WAITING_FOREVER, &recv_event);
-    DEBUG_PIN_DEBUG3_LOW();
+    rt_event_recv(stabilizer_event, STABILIZER_EVENT_FLAG, RT_EVENT_FLAG_OR | RT_EVENT_FLAG_CLEAR, RT_WAITING_FOREVER,
+                  RT_NULL);
 
-    if (result == RT_EOK) {
-      if (RATE_DO_EXECUTE(ATTITUDE_ESTIMAT_RATE, tick)) {
-        sensorsAcquire(&sensorData);
-        imuUpdate(sensorData.acc_filter, sensorData.gyro_filter, &state_minifly_, ATTITUDE_ESTIMAT_DT);
-        mcn_publish(MCN_HUB(minifly_stabilizer_state), &state_minifly_);
-      }
-
-      tick++;
+    if (RATE_DO_EXECUTE(ATTITUDE_ESTIMAT_RATE, tick)) {
+#ifdef PROJECT_MINIFLY_TASK_DEBUGPIN_EN
+      DEBUG_PIN_DEBUG3_HIGH();
+#endif
+      sensorsAcquire(&sensorData);
+      imuUpdate(sensorData.acc_filter, sensorData.gyro_filter, &state_minifly_, ATTITUDE_ESTIMAT_DT);
+      mcn_publish(MCN_HUB(minifly_stabilizer_state), &state_minifly_);
+#ifdef PROJECT_MINIFLY_TASK_DEBUGPIN_EN
+      DEBUG_PIN_DEBUG3_LOW();
+#endif
     }
+
+    tick++;
   }
 }
 
