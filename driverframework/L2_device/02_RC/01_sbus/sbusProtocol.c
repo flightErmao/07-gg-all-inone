875f0585ba3fd00585a60303263c2e8b390711d2
Gaogao <1024355591@qq.com>
2025-09-09 11:37:11 +0800

    sbus编译成功

---
+++ driverframework/L2_device/02_RC/01_sbus/sbusProtocol.c
@@ -0,0 +1,119 @@
+#include "sbusProtocol.h"
+
+static const struct sbus_bit_pick sbus_decoder[SBUS_INPUT_CHANNELS][3] = {
+    /*  0 */ {{0, 0, 0xff, 0}, {1, 0, 0x07, 8}, {0, 0, 0x00, 0}},
+    /*  1 */ {{1, 3, 0x1f, 0}, {2, 0, 0x3f, 5}, {0, 0, 0x00, 0}},
+    /*  2 */ {{2, 6, 0x03, 0}, {3, 0, 0xff, 2}, {4, 0, 0x01, 10}},
+    /*  3 */ {{4, 1, 0x7f, 0}, {5, 0, 0x0f, 7}, {0, 0, 0x00, 0}},
+    /*  4 */ {{5, 4, 0x0f, 0}, {6, 0, 0x7f, 4}, {0, 0, 0x00, 0}},
+    /*  5 */ {{6, 7, 0x01, 0}, {7, 0, 0xff, 1}, {8, 0, 0x03, 9}},
+    /*  6 */ {{8, 2, 0x3f, 0}, {9, 0, 0x1f, 6}, {0, 0, 0x00, 0}},
+    /*  7 */ {{9, 5, 0x07, 0}, {10, 0, 0xff, 3}, {0, 0, 0x00, 0}},
+    /*  8 */ {{11, 0, 0xff, 0}, {12, 0, 0x07, 8}, {0, 0, 0x00, 0}},
+    /*  9 */ {{12, 3, 0x1f, 0}, {13, 0, 0x3f, 5}, {0, 0, 0x00, 0}},
+    /* 10 */ {{13, 6, 0x03, 0}, {14, 0, 0xff, 2}, {15, 0, 0x01, 10}},
+    /* 11 */ {{15, 1, 0x7f, 0}, {16, 0, 0x0f, 7}, {0, 0, 0x00, 0}},
+    /* 12 */ {{16, 4, 0x0f, 0}, {17, 0, 0x7f, 4}, {0, 0, 0x00, 0}},
+    /* 13 */ {{17, 7, 0x01, 0}, {18, 0, 0xff, 1}, {19, 0, 0x03, 9}},
+    /* 14 */ {{19, 2, 0x3f, 0}, {20, 0, 0x1f, 6}, {0, 0, 0x00, 0}},
+    /* 15 */ {{20, 5, 0x07, 0}, {21, 0, 0xff, 3}, {0, 0, 0x00, 0}}};
+
+rt_err_t sbus_decoder_init(sbus_decoder_t* decoder) {
+  if (decoder == NULL) {
+    return RT_EINVAL;
+  }
+
+  rt_memset(decoder, 0, sizeof(sbus_decoder_t));
+
+  decoder->max_channels = MAX_SBUS_CHANNEL;
+
+  static uint8_t sbus_rb_pool[SBUS_FRAME_SIZE * 4];
+  decoder->sbus_rb = rt_malloc(sizeof(struct rt_ringbuffer));
+  if (decoder->sbus_rb == NULL) {
+    rt_kprintf("[SBUS] malloc ringbuffer failed\n");
+    return RT_ENOMEM;
+  }
+  rt_ringbuffer_init(decoder->sbus_rb, sbus_rb_pool, sizeof(sbus_rb_pool));
+
+  static struct rt_event event_data_received;
+  static struct rt_event event_data_ready;
+  rt_event_init(&event_data_received, "sbus_rx", RT_IPC_FLAG_FIFO);
+  rt_event_init(&event_data_ready, "sbus_rdy", RT_IPC_FLAG_FIFO);
+  decoder->sbus_data_received_event = &event_data_received;
+  decoder->sbus_data_ready_event = &event_data_ready;
+
+  static struct rt_mutex sbus_mutex;
+  rt_mutex_init(&sbus_mutex, "sbus_mtx", RT_IPC_FLAG_FIFO);
+  decoder->sbus_mutex = &sbus_mutex;
+
+  return RT_EOK;
+}
+
+bool sbus_parse(sbus_decoder_t* decoder, uint8_t* frame, unsigned len) {
+  if (len != SBUS_FRAME_SIZE) {
+    decoder->sbus_frame_drops++;
+    return false;
+  }
+
+  if (frame[0] != SBUS_START_SYMBOL) {
+    decoder->sbus_frame_drops++;
+    return false;
+  }
+
+  unsigned chancount = (decoder->max_channels > SBUS_INPUT_CHANNELS) ? SBUS_INPUT_CHANNELS : decoder->max_channels;
+
+  for (unsigned channel = 0; channel < chancount; channel++) {
+    unsigned value = 0;
+
+    for (unsigned pick = 0; pick < 3; pick++) {
+      const struct sbus_bit_pick* decode = &sbus_decoder[channel][pick];
+
+      if (decode->mask != 0) {
+        unsigned piece = frame[1 + decode->byte];
+        piece >>= decode->rshift;
+        piece &= decode->mask;
+        piece <<= decode->lshift;
+
+        value |= piece;
+      }
+    }
+
+    decoder->sbus_val[channel] = (uint16_t)(value * SBUS_SCALE_FACTOR + .5f) + SBUS_SCALE_OFFSET;
+  }
+
+  if (decoder->max_channels > 17 && chancount > 15) {
+    chancount = 18;
+    decoder->sbus_val[16] = (frame[SBUS_FLAGS_BYTE] & (1 << 0)) * 1000 + 998;
+    decoder->sbus_val[17] = (frame[SBUS_FLAGS_BYTE] & (1 << 1)) * 1000 + 998;
+  }
+
+  decoder->rc_count = chancount;
+  decoder->sbus_failsafe = (frame[SBUS_FLAGS_BYTE] & (1 << SBUS_FAILSAFE_BIT));
+  decoder->sbus_frame_drop = (frame[SBUS_FLAGS_BYTE] & (1 << SBUS_FRAMELOST_BIT));
+
+  return true;
+}
+
+bool sbus_update(sbus_decoder_t* decoder) {
+  int ret;
+
+  uint8_t buf[SBUS_FRAME_SIZE];
+  ret = rt_ringbuffer_get(decoder->sbus_rb, buf, SBUS_FRAME_SIZE);
+
+  if (ret < SBUS_FRAME_SIZE) {
+    return false;
+  }
+
+  bool sbus_updated = sbus_parse(decoder, buf, ret);
+  decoder->sbus_data_ready = sbus_updated && !decoder->sbus_failsafe && !decoder->sbus_frame_drop;
+
+  if (decoder->sbus_data_ready && decoder->sbus_data_ready_event != NULL) {
+    rt_event_send(decoder->sbus_data_ready_event, EVENT_SBUS_DATA_READY);
+  }
+
+  return decoder->sbus_data_ready;
+}
+
+uint32_t sbus_input(sbus_decoder_t* decoder, const uint8_t* values, uint32_t size) {
+  return rt_ringbuffer_put(decoder->sbus_rb, values, size);
+}
---
+++ driverframework/L2_device/02_RC/01_sbus/sbusProtocol.h
@@ -0,0 +1,87 @@
+#ifndef SBUS_PROTOCOL_H
+#define SBUS_PROTOCOL_H
+
+#include <rtthread.h>
+#include <rtdevice.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MAX_SBUS_CHANNEL 16
+#define SBUS_FRAME_SIZE 25
+#define SBUS_START_SYMBOL 0x0f
+#define SBUS_END_SYMBOL 0x00
+#define SBUS_INPUT_CHANNELS 16
+#define SBUS_FLAGS_BYTE 23
+#define SBUS_FAILSAFE_BIT 3
+#define SBUS_FRAMELOST_BIT 2
+
+#define SBUS_RANGE_MIN 200.0f
+#define SBUS_RANGE_MAX 1800.0f
+#define SBUS_TARGET_MIN 1000.0f
+#define SBUS_TARGET_MAX 2000.0f
+#define SBUS_SCALE_FACTOR ((SBUS_TARGET_MAX - SBUS_TARGET_MIN) / (SBUS_RANGE_MAX - SBUS_RANGE_MIN))
+#define SBUS_SCALE_OFFSET (int)(SBUS_TARGET_MIN - (SBUS_SCALE_FACTOR * SBUS_RANGE_MIN + 0.5f))
+
+#define SBUS_INVALID_CHANNEL_VALUE 1500
+#define SBUS_READ_TIMEOUT_MS 100
+
+#define EVENT_SBUS_DATA_RECEIVED (1 << 0)
+#define EVENT_SBUS_DATA_READY (1 << 1)
+
+struct sbus_bit_pick {
+  uint8_t byte;
+  uint8_t rshift;
+  uint8_t mask;
+  uint8_t lshift;
+};
+
+typedef struct {
+  uint16_t rc_count;
+  uint16_t max_channels;
+  bool sbus_failsafe;
+  bool sbus_frame_drop;
+  uint32_t sbus_frame_drops;
+  bool sbus_data_ready;
+  uint8_t sbus_lock;
+  struct rt_ringbuffer* sbus_rb;
+  uint16_t sbus_val[MAX_SBUS_CHANNEL];
+  struct rt_event* sbus_data_received_event;
+  struct rt_event* sbus_data_ready_event;
+  struct rt_mutex* sbus_mutex;
+} sbus_decoder_t;
+
+rt_inline void sbus_lock(sbus_decoder_t* decoder) {
+  if (decoder->sbus_mutex) {
+    rt_mutex_take(decoder->sbus_mutex, RT_WAITING_FOREVER);
+  }
+  decoder->sbus_lock = 1;
+}
+
+rt_inline void sbus_unlock(sbus_decoder_t* decoder) {
+  decoder->sbus_lock = 0;
+  if (decoder->sbus_mutex) {
+    rt_mutex_release(decoder->sbus_mutex);
+  }
+}
+
+rt_inline uint8_t sbus_islock(sbus_decoder_t* decoder) { return decoder->sbus_lock; }
+
+rt_inline uint8_t sbus_data_ready(sbus_decoder_t* decoder) { return decoder->sbus_data_ready; }
+
+rt_inline void sbus_data_clear(sbus_decoder_t* decoder) { decoder->sbus_data_ready = 0; }
+
+/* Function declarations */
+rt_err_t sbus_decoder_init(sbus_decoder_t* decoder);
+bool sbus_parse(sbus_decoder_t* decoder, uint8_t* frame, unsigned len);
+bool sbus_update(sbus_decoder_t* decoder);
+uint32_t sbus_input(sbus_decoder_t* decoder, const uint8_t* values, uint32_t size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SBUS_PROTOCOL_H */
