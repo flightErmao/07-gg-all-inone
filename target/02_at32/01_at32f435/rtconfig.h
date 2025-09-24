d1f27036e147ededc91ea221c016b45cd41d7d6d
gun <gf1024355591@163.com>
2025-09-24 20:31:01 +0800

    优化at32的最小bsp模板

--- target/02_at32/01_at32f435/rtconfig.h
+++ target/02_at32/01_at32f435/rtconfig.h
@@ -228,6 +228,20 @@
 
 /* Using USB legacy version */
 
+#define RT_USING_USB
+#define RT_USING_USB_DEVICE
+#define RT_USBD_THREAD_STACK_SZ 4096
+#define USB_VENDOR_ID 0x0FFE
+#define USB_PRODUCT_ID 0x0001
+#define RT_USB_DEVICE_COMPOSITE
+#define RT_USB_DEVICE_CDC
+#define RT_USB_DEVICE_NONE
+#define RT_VCOM_TASK_STK_SIZE 512
+#define RT_CDC_RX_BUFSIZE 128
+#define RT_VCOM_TX_USE_DMA
+#define RT_VCOM_SERNO "32021919830108"
+#define RT_VCOM_SER_LEN 14
+#define RT_VCOM_TX_TIMEOUT 1000
 /* end of Using USB legacy version */
 /* end of RT-Thread Components */
 
@@ -296,6 +310,8 @@
 
 /* tools packages */
 
+#define PKG_USING_VCONSOLE
+#define PKG_USING_VCONSOLE_LATEST_VERSION
 /* end of tools packages */
 
 /* system packages */
@@ -522,6 +538,13 @@
 #define BSP_UART3_RX_BUFSIZE 256
 #define BSP_UART3_TX_BUFSIZE 64
 #define BSP_UART3_DMA_PING_BUFSIZE 64
+
+/* Enable 14 USB CONFIG */
+
+#define BSP_USING_USBOTG
+#define BSP_USING_USBOTG1
+#define BSP_USING_DEVICE_USBOTG1
+/* end of Enable 14 USB CONFIG */
 #define BSP_USING_SDIO
 #define BSP_USING_SDIO1
 #define BSP_USING_SDCARD
