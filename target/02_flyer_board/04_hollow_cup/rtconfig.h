f1ef6838f8103a5024e182b0116ebd505253de0d
gun <gf1024355591@163.com>
2025-10-23 23:05:17 +0800

    添加空心杯的target

---
+++ target/02_flyer_board/04_hollow_cup/rtconfig.h
@@ -0,0 +1,636 @@
+#ifndef RT_CONFIG_H__
+#define RT_CONFIG_H__
+
+#define SOC_AT32F435ZMT7
+
+/* RT-Thread Kernel */
+
+/* klibc options */
+
+/* rt_vsnprintf options */
+
+/* end of rt_vsnprintf options */
+
+/* rt_vsscanf options */
+
+/* end of rt_vsscanf options */
+
+/* rt_memset options */
+
+/* end of rt_memset options */
+
+/* rt_memcpy options */
+
+/* end of rt_memcpy options */
+
+/* rt_memmove options */
+
+/* end of rt_memmove options */
+
+/* rt_memcmp options */
+
+/* end of rt_memcmp options */
+
+/* rt_strstr options */
+
+/* end of rt_strstr options */
+
+/* rt_strcasecmp options */
+
+/* end of rt_strcasecmp options */
+
+/* rt_strncpy options */
+
+/* end of rt_strncpy options */
+
+/* rt_strcpy options */
+
+/* end of rt_strcpy options */
+
+/* rt_strncmp options */
+
+/* end of rt_strncmp options */
+
+/* rt_strcmp options */
+
+/* end of rt_strcmp options */
+
+/* rt_strlen options */
+
+/* end of rt_strlen options */
+
+/* rt_strnlen options */
+
+/* end of rt_strnlen options */
+/* end of klibc options */
+#define RT_NAME_MAX 24
+#define RT_CPUS_NR 1
+#define RT_ALIGN_SIZE 8
+#define RT_THREAD_PRIORITY_32
+#define RT_THREAD_PRIORITY_MAX 32
+#define RT_TICK_PER_SECOND 1000
+#define RT_USING_OVERFLOW_CHECK
+#define RT_USING_HOOK
+#define RT_HOOK_USING_FUNC_PTR
+#define RT_USING_IDLE_HOOK
+#define RT_IDLE_HOOK_LIST_SIZE 4
+#define IDLE_THREAD_STACK_SIZE 256
+
+/* kservice options */
+
+/* end of kservice options */
+#define RT_USING_DEBUG
+#define RT_DEBUGING_ASSERT
+#define RT_DEBUGING_COLOR
+#define RT_DEBUGING_CONTEXT
+
+/* Inter-Thread communication */
+
+#define RT_USING_SEMAPHORE
+#define RT_USING_MUTEX
+#define RT_USING_EVENT
+#define RT_USING_MAILBOX
+#define RT_USING_MESSAGEQUEUE
+/* end of Inter-Thread communication */
+
+/* Memory Management */
+
+#define RT_USING_MEMPOOL
+#define RT_USING_SMALL_MEM
+#define RT_USING_MEMHEAP
+#define RT_MEMHEAP_FAST_MODE
+#define RT_USING_SMALL_MEM_AS_HEAP
+#define RT_USING_HEAP
+/* end of Memory Management */
+#define RT_USING_DEVICE
+#define RT_USING_CONSOLE
+#define RT_CONSOLEBUF_SIZE 128
+#define RT_CONSOLE_DEVICE_NAME "uart2"
+#define RT_VER_NUM 0x50201
+#define RT_BACKTRACE_LEVEL_MAX_NR 32
+/* end of RT-Thread Kernel */
+#define RT_USING_HW_ATOMIC
+#define RT_USING_CPU_FFS
+#define ARCH_ARM
+#define ARCH_ARM_CORTEX_M
+#define ARCH_ARM_CORTEX_M4
+
+/* RT-Thread Components */
+
+#define RT_USING_COMPONENTS_INIT
+#define RT_USING_USER_MAIN
+#define RT_MAIN_THREAD_STACK_SIZE 2048
+#define RT_MAIN_THREAD_PRIORITY 10
+#define RT_USING_LEGACY
+#define RT_USING_MSH
+#define RT_USING_FINSH
+#define FINSH_USING_MSH
+#define FINSH_THREAD_NAME "tshell"
+#define FINSH_THREAD_PRIORITY 20
+#define FINSH_THREAD_STACK_SIZE 4096
+#define FINSH_USING_HISTORY
+#define FINSH_HISTORY_LINES 5
+#define FINSH_USING_SYMTAB
+#define FINSH_CMD_SIZE 80
+#define MSH_USING_BUILT_IN_COMMANDS
+#define FINSH_USING_DESCRIPTION
+#define FINSH_ARG_MAX 10
+#define FINSH_USING_OPTION_COMPLETION
+
+/* DFS: device virtual file system */
+
+#define RT_USING_DFS
+#define DFS_USING_POSIX
+#define DFS_USING_WORKDIR
+#define DFS_FD_MAX 16
+#define RT_USING_DFS_V1
+#define DFS_FILESYSTEMS_MAX 4
+#define DFS_FILESYSTEM_TYPES_MAX 4
+#define RT_USING_DFS_ELMFAT
+
+/* elm-chan's FatFs, Generic FAT Filesystem Module */
+
+#define RT_DFS_ELM_CODE_PAGE 437
+#define RT_DFS_ELM_WORD_ACCESS
+#define RT_DFS_ELM_USE_LFN_3
+#define RT_DFS_ELM_USE_LFN 3
+#define RT_DFS_ELM_LFN_UNICODE_0
+#define RT_DFS_ELM_LFN_UNICODE 0
+#define RT_DFS_ELM_MAX_LFN 255
+#define RT_DFS_ELM_DRIVES 2
+#define RT_DFS_ELM_MAX_SECTOR_SIZE 512
+#define RT_DFS_ELM_REENTRANT
+#define RT_DFS_ELM_MUTEX_TIMEOUT 3000
+#define RT_DFS_ELM_USE_EXFAT
+/* end of elm-chan's FatFs, Generic FAT Filesystem Module */
+#define RT_USING_DFS_DEVFS
+/* end of DFS: device virtual file system */
+#define RT_USING_FAL
+#define FAL_USING_DEBUG
+#define FAL_PART_HAS_TABLE_CFG
+
+/* Device Drivers */
+
+#define RT_USING_DEVICE_IPC
+#define RT_UNAMED_PIPE_NUMBER 64
+#define RT_USING_SERIAL
+#define RT_USING_SERIAL_V2
+#define RT_SERIAL_BUF_STRATEGY_OVERWRITE
+#define RT_SERIAL_USING_DMA
+#define RT_USING_SDIO
+#define RT_SDIO_STACK_SIZE 2048
+#define RT_SDIO_THREAD_PRIORITY 15
+#define RT_MMCSD_STACK_SIZE 2048
+#define RT_MMCSD_THREAD_PRIORITY 22
+#define RT_MMCSD_MAX_PARTITION 16
+#define RT_USING_SPI
+#define RT_USING_BLK
+
+/* Partition Types */
+
+#define RT_BLK_PARTITION_DFS
+#define RT_BLK_PARTITION_EFI
+/* end of Partition Types */
+#define RT_USING_PIN
+#define RT_USING_KTIME
+/* end of Device Drivers */
+
+/* C/C++ and POSIX layer */
+
+/* ISO-ANSI C layer */
+
+/* Timezone and Daylight Saving Time */
+
+#define RT_LIBC_USING_LIGHT_TZ_DST
+#define RT_LIBC_TZ_DEFAULT_HOUR 8
+#define RT_LIBC_TZ_DEFAULT_MIN 0
+#define RT_LIBC_TZ_DEFAULT_SEC 0
+/* end of Timezone and Daylight Saving Time */
+/* end of ISO-ANSI C layer */
+
+/* POSIX (Portable Operating System Interface) layer */
+
+#define RT_USING_POSIX_DELAY
+
+/* Interprocess Communication (IPC) */
+
+
+/* Socket is in the 'Network' category */
+
+/* end of Interprocess Communication (IPC) */
+/* end of POSIX (Portable Operating System Interface) layer */
+#define RT_USING_CPLUSPLUS
+/* end of C/C++ and POSIX layer */
+
+/* Network */
+
+/* end of Network */
+
+/* Memory protection */
+
+/* end of Memory protection */
+
+/* Utilities */
+
+/* end of Utilities */
+
+/* Using USB legacy version */
+
+/* end of Using USB legacy version */
+/* end of RT-Thread Components */
+
+/* RT-Thread Utestcases */
+
+/* end of RT-Thread Utestcases */
+
+/* RT-Thread online packages */
+
+/* IoT - internet of things */
+
+
+/* Wi-Fi */
+
+/* Marvell WiFi */
+
+/* end of Marvell WiFi */
+
+/* Wiced WiFi */
+
+/* end of Wiced WiFi */
+
+/* CYW43012 WiFi */
+
+/* end of CYW43012 WiFi */
+
+/* BL808 WiFi */
+
+/* end of BL808 WiFi */
+
+/* CYW43439 WiFi */
+
+/* end of CYW43439 WiFi */
+/* end of Wi-Fi */
+
+/* IoT Cloud */
+
+/* end of IoT Cloud */
+/* end of IoT - internet of things */
+
+/* security packages */
+
+/* end of security packages */
+
+/* language packages */
+
+/* JSON: JavaScript Object Notation, a lightweight data-interchange format */
+
+/* end of JSON: JavaScript Object Notation, a lightweight data-interchange format */
+
+/* XML: Extensible Markup Language */
+
+/* end of XML: Extensible Markup Language */
+/* end of language packages */
+
+/* multimedia packages */
+
+/* LVGL: powerful and easy-to-use embedded GUI library */
+
+/* end of LVGL: powerful and easy-to-use embedded GUI library */
+
+/* u8g2: a monochrome graphic library */
+
+/* end of u8g2: a monochrome graphic library */
+/* end of multimedia packages */
+
+/* tools packages */
+
+#define PKG_USING_UMCN
+#define PKG_USING_UMCN_LATEST_VERSION
+#define UMCN_USING_CMD
+/* end of tools packages */
+
+/* system packages */
+
+/* enhanced kernel services */
+
+/* end of enhanced kernel services */
+
+/* acceleration: Assembly language or algorithmic acceleration packages */
+
+/* end of acceleration: Assembly language or algorithmic acceleration packages */
+
+/* CMSIS: ARM Cortex-M Microcontroller Software Interface Standard */
+
+#define PKG_USING_CMSIS_CORE
+#define PKG_USING_CMSIS_CORE_LATEST_VERSION
+/* end of CMSIS: ARM Cortex-M Microcontroller Software Interface Standard */
+
+/* Micrium: Micrium software products porting for RT-Thread */
+
+/* end of Micrium: Micrium software products porting for RT-Thread */
+/* end of system packages */
+
+/* peripheral libraries and drivers */
+
+/* HAL & SDK Drivers */
+
+/* STM32 HAL & SDK Drivers */
+
+/* end of STM32 HAL & SDK Drivers */
+
+/* Infineon HAL Packages */
+
+/* end of Infineon HAL Packages */
+
+/* Kendryte SDK */
+
+/* end of Kendryte SDK */
+
+/* WCH HAL & SDK Drivers */
+
+/* end of WCH HAL & SDK Drivers */
+
+/* AT32 HAL & SDK Drivers */
+
+#define PKG_USING_AT32F435_437_HAL_DRIVER
+#define PKG_USING_AT32F435_437_HAL_DRIVER_LATEST_VERSION
+#define PKG_USING_AT32F435_437_CMSIS_DRIVER
+#define PKG_USING_AT32F435_437_CMSIS_DRIVER_LATEST_VERSION
+/* end of AT32 HAL & SDK Drivers */
+
+/* HC32 DDL Drivers */
+
+/* end of HC32 DDL Drivers */
+
+/* NXP HAL & SDK Drivers */
+
+/* end of NXP HAL & SDK Drivers */
+
+/* NUVOTON Drivers */
+
+/* end of NUVOTON Drivers */
+/* end of HAL & SDK Drivers */
+
+/* sensors drivers */
+
+/* end of sensors drivers */
+
+/* touch drivers */
+
+/* end of touch drivers */
+/* end of peripheral libraries and drivers */
+
+/* AI packages */
+
+/* end of AI packages */
+
+/* Signal Processing and Control Algorithm Packages */
+
+/* end of Signal Processing and Control Algorithm Packages */
+
+/* miscellaneous packages */
+
+/* project laboratory */
+
+/* end of project laboratory */
+
+/* samples: kernel and components samples */
+
+/* end of samples: kernel and components samples */
+
+/* entertainment: terminal games and other interesting software packages */
+
+/* end of entertainment: terminal games and other interesting software packages */
+#define PKG_USING_OPTPARSE
+#define PKG_USING_OPTPARSE_LATEST_VERSION
+/* end of miscellaneous packages */
+
+/* Arduino libraries */
+
+
+/* Projects and Demos */
+
+/* end of Projects and Demos */
+
+/* Sensors */
+
+/* end of Sensors */
+
+/* Display */
+
+/* end of Display */
+
+/* Timing */
+
+/* end of Timing */
+
+/* Data Processing */
+
+/* end of Data Processing */
+
+/* Data Storage */
+
+/* Communication */
+
+/* end of Communication */
+
+/* Device Control */
+
+/* end of Device Control */
+
+/* Other */
+
+/* end of Other */
+
+/* Signal IO */
+
+/* end of Signal IO */
+
+/* Uncategorized */
+
+/* end of Arduino libraries */
+/* end of RT-Thread online packages */
+#define SOC_FAMILY_AT32
+#define SOC_SERIES_AT32F435
+
+/* DriverFramework Config */
+
+/* L0_TASK_CONFIG */
+
+#define TASK_TOOL_01_ANOTC_TELEM_EN
+#define TASK_TOOL_01_ANOTC_TELEM_DEVICE_DEFAULT "uart1"
+#define TASK_TOOL_01_ANOTC_TELEM_BAUD_RATE 500000
+#define TASK_TOOL_02_SD_MLOG
+#define PROJECT_MINIFLY_TASK_SENSOR_EN
+#define PROJECT_MINIFLY_TASK_SENSOR_IMU_NAME "bmi270"
+#define PROJECT_MINIFLY_TASK_SENSOR_TIMER_TRIGGER_EN
+#define PROJECT_MINIFLY_TASK_SENSOR_ROTATION "ROTATION_ROLL_180"
+#define PROJECT_MINIFLY_TASK_SENSOR_GYRO_VARIANCE_BASE 5000
+#define PROJECT_MINIFLY_TASK_SENSOR_MLOG_EN
+#define PROJECT_MINIFLY_TASK_SENSOR_MLOG_IMU_EN
+#define PROJECT_MINIFLY_TASK_STABLIZE_EN
+#define PROJECT_MINIFLY_TASK_STABLIZE_ATKP_LOG_EN
+#define PROJECT_MINIFLY_TASK_STABLIZE_LOG_ANGLE_DEBUG
+#define PROJECT_MINIFLY_TASK_STABLIZE_LOG_GROUP_ANGLE 1
+#define PROJECT_MINIFLY_TASK_STABLIZE_LOG_ANGLE_PERIOD_MS 20
+#define PROJECT_MINIFLY_TASK_STABLIZE_LOG_RATE_DEBUG
+#define PROJECT_MINIFLY_TASK_STABLIZE_LOG_GROUP_RATE 3
+#define PROJECT_MINIFLY_TASK_STABLIZE_LOG_RATE_PERIOD_MS 20
+#define PROJECT_MINIFLY_TASK_STABLIZE_MLOG_EN
+#define PROJECT_MINIFLY_TASK_STABLIZE_MLOG_PID_EN
+#define PROJECT_MINIFLY_TASK_STABLIZE_MLOG_ANGLE_PID_EN
+#define PROJECT_MINIFLY_TASK_STABLIZE_MLOG_RATE_PID_EN
+#define PROJECT_MINIFLY_TASK_STABLIZE_MLOG_RATE_EN
+#define PROJECT_MINIFLY_TASK_STABLIZE_MLOG_ANGLE_EN
+#define PROJECT_MINIFLY_TASK_STABLIZE_MLOG_RC_EN
+#define PROJECT_MINIFLY_TASK_STABLIZE_RC_FILTER_EN
+#define PROJECT_MINIFLY_TASK_STABLIZE_RC_FILTER_ALPHA 6
+#define PROJECT_MINIFLY_TASK04_DISTRIBUTE_EN
+#define PROJECT_MINIFLY_TASK04_DISTRIBUTE_ACK_EN
+#define PROJECT_MINIFLY_TASK04_DISTRIBUTE_PID_EN
+#define PROJECT_MINIFLY_TASK05_PARAM_EN
+#define PROJECT_MINIFLY_TASK05_PARAM_AUTO_SAVE_EN
+#define PROJECT_FMT_TASK01_RC_EN
+#define PROJECT_FMT_TASK01_RC_DEVICE_DEFAULT "sbus"
+#define PROJECT_FMT_TASK01_RC_DEADZONE_PERCENT 3
+#define PROJECT_MINIFLY_TASK_DSHOT_EN
+#define PROJECT_MINIFLY_TASK_DSHOT_TIMER_HZ 500
+#define PROJECT_MINIFLY_TASK_DSHOT_MLOG_EN
+#define PROJECT_MINIFLY_TASK_DSHOT_MOTOR_FILTER_EN
+/* end of L0_TASK_CONFIG */
+
+/* L1_MIDDLEWARE_CONFIG */
+
+#define L1_MIDDLEWARE_01_MODULE_01_FLOATCONVER_EN
+#define L1_MIDDLEWARE_01_MODULE_02_MATHS_EN
+#define L1_MIDDLEWARE_01_MODULE_03_DEBUGPIN_EN
+#define L1_MIDDLEWARE_01_MODULE_03_DEBUGPIN_0_EN
+#define L1_MIDDLEWARE_01_MODULE_03_DEBUGPIN_0_PIN "PC1"
+#define L1_MIDDLEWARE_01_MODULE_03_DEBUGPIN_1_EN
+#define L1_MIDDLEWARE_01_MODULE_03_DEBUGPIN_1_PIN "PC0"
+#define L1_MIDDLEWARE_01_MODULE_03_DEBUGPIN_2_EN
+#define L1_MIDDLEWARE_01_MODULE_03_DEBUGPIN_2_PIN "PC2"
+#define L1_MIDDLEWARE_01_MODULE_03_DEBUGPIN_3_EN
+#define L1_MIDDLEWARE_01_MODULE_03_DEBUGPIN_3_PIN "PC3"
+#define L1_MIDDLEWARE_01_MODULE_03_DEBUGPIN_TESTSELF_EN
+#define L1_MIDDLEWARE_01_MODULE_04_PID_MINIFLY_EN
+#define L1_MIDDLEWARE_01_MODULE_05_FILTER_RPM_EN
+/* end of L1_MIDDLEWARE_CONFIG */
+
+/* L2_DEVICE_CONFIG */
+
+/* 01 IMU CONFIG */
+
+/* 01 ICM42688_130 CONFIG */
+
+/* end of 01 ICM42688_130 CONFIG */
+
+/* 02 MPU6500_minifly CONFIG */
+
+/* end of 02 MPU6500_minifly CONFIG */
+
+/* 03 MPU6000_FMUV2 CONFIG */
+
+/* end of 03 MPU6000_FMUV2 CONFIG */
+
+/* 04 BMI270 CONFIG */
+
+#define BSP_USING_BMI270
+#define SENSOR_NAME_BMI270 "bmi270"
+#define SENSOR_SPI_NAME_BMI270 "spi1"
+#define SENSOR_SPI_SLAVE_NAME_BMI270 "spi10"
+#define SENSOR_BMI270_SPI_MAX_HZ 10000000
+#define SENSOR_BMI270_SPI_CS_PIN "PA4"
+/* end of 04 BMI270 CONFIG */
+/* end of 01 IMU CONFIG */
+
+/* 02 RC CONFIG */
+
+/* 01 SBUS CONFIG */
+
+#define RC_USING_SBUS
+#define RC_SBUS_UART_NAME "uart3"
+#define RC_SBUS_BAUDRATE 100000
+#define RC_SBUS_INVERTED
+/* end of 01 SBUS CONFIG */
+
+/* 02 CRSF CONFIG */
+
+/* end of 02 CRSF CONFIG */
+/* end of 02 RC CONFIG */
+
+/* 03 MOTOR CONFIG */
+
+/* 01 PWM CONFIG(Hollow slab) */
+
+/* end of 01 PWM CONFIG(Hollow slab) */
+
+/* 02 DSHOT CONFIG */
+
+#define L2_DEVICE_03_MOTOR_02_DSHOT_EN
+#define DSHOT_DEVICE_NAME "dshot"
+#define DSHOT_PROTOCOL_TYPE "dshot_600"
+#define DSHOT_MOTOR_NUMS 4
+#define DSHOT_GPIO_PORT "GPIOB"
+#define DSHOT_GPIO_PIN_MOTOR1 9
+#define DSHOT_GPIO_PIN_MOTOR2 8
+#define DSHOT_GPIO_PIN_MOTOR3 6
+#define DSHOT_GPIO_PIN_MOTOR4 7
+#define DSHOT_TIMER_SELECT 4
+#define DSHOT_BI_DIRECTIONAL
+#define DSHOT_CMD_MOTOR_EN
+/* end of 02 DSHOT CONFIG */
+
+/* 03 PWM CONFIG */
+
+/* end of 03 PWM CONFIG */
+/* end of 03 MOTOR CONFIG */
+
+/* 05 MAG CONFIG */
+
+/* 01 QMC5883L CONFIG */
+
+/* end of 01 QMC5883L CONFIG */
+
+/* 02 QMC6308 CONFIG */
+
+/* end of 02 QMC6308 CONFIG */
+/* end of 05 MAG CONFIG */
+/* end of L2_DEVICE_CONFIG */
+
+/* L3_PERIPHERAL_CONFIG */
+
+#define L3_PERIPHERAL_02_NVIC_CONFIG_EN
+#define L3_PERIPHERAL_02_NVIC_STM32_PRIORITY_GROUP 0
+#define BSP_USING_GPIO
+#define BSP_USING_UART
+#define BSP_USING_UART1
+#define BSP_UART1_RX_USING_DMA
+#define BSP_UART1_TX_USING_DMA
+#define BSP_UART1_RX_BUFSIZE 256
+#define BSP_UART1_TX_BUFSIZE 64
+#define BSP_UART1_DMA_PING_BUFSIZE 64
+#define BSP_USING_UART2
+#define BSP_UART2_RX_BUFSIZE 256
+#define BSP_UART2_TX_BUFSIZE 64
+#define BSP_USING_UART3
+#define BSP_UART3_RX_USING_DMA
+#define BSP_UART3_TX_USING_DMA
+#define BSP_UART3_RX_BUFSIZE 256
+#define BSP_UART3_TX_BUFSIZE 64
+#define BSP_UART3_DMA_PING_BUFSIZE 64
+#define BSP_USING_SPI
+#define BSP_USING_SPI1
+
+/* Enable 14 USB CONFIG */
+
+/* end of Enable 14 USB CONFIG */
+#define BSP_USING_ON_CHIP_FLASH
+#define L3_PERIPHERAL_USING_ON_CHIP_FLASH_AUTOSTART_FALINIT
+#define BSP_USING_SDIO
+#define BSP_USING_SDIO1
+#define BSP_USING_SDCARD
+/* end of L3_PERIPHERAL_CONFIG */
+/* end of DriverFramework Config */
+
+#endif
